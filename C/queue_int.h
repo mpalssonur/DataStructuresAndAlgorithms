c1df3ba2d21013e10af2fef3667b7acd5cc8d145
mpalssonur <mpalssonur@gmail.com>
2025-01-10 01:32:48 +0000

    Added documentation comments to queue header

--- C/queue_int.h
+++ C/queue_int.h
@@ -13,18 +13,35 @@ struct Queue {
     int length;
 };
 
+// Allocate memory for queue, 
+// initialise tail and head to NULL and 
+// set length to 0
+// Returns pointer to queue
 struct Queue* init_queue();
 
+// Allocate memory for node in queue
+// with data=value. 
+// Initialize next and prev to NULL
+// Return pointer to node
 struct Node* create_node(int value);
 
+// Enqueue value at head of queue
 void enqueue(struct Queue* queue, int value);
 
+// Dequeue from the tail of the queue
+// Return data of dequeued node
+// Returns 0 if queue is empty
+// User is expected to check if queue is empty
 int dequeue(struct Queue* queue);
 
+// Removes all nodes from queue
 void clear_queue(struct Queue* queue);
 
+// clears the queue and frees the memory
 void free_queue(struct Queue* queue);
 
+// prints the contents of the queue
+// for testing and debugging
 void print_queue(struct Queue* queue);
 
 #endif
